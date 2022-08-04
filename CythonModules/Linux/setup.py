e8a190d4129058d01629163ceae7263f5fd42deb
Iréné <amieheessomba@etu.unistra.fr>
2022-08-04 09:33:00 +0200

    new modules Cython

---
+++ CythonModules/Linux/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, Extension
+from Cython.Build import  cythonize
+
+
+
+setup(
+    name = "my cython modules",
+    author='amiehe',
+    author_email='amieheessomba@etu.unistra.fr',
+    ext_modules=cythonize( 
+        [
+            "making_stat.pyx", 
+            "loop_for.pyx",
+            'NumeriCal.pyx',
+            'arithmetic_analyze.pyx',
+            'making_arr.pyx',
+            'fileError.pyx'
+            ], 
+        annotate = True
+        )
+
+)
---
+++ CythonModules/Windows/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, Extension
+from Cython.Build import  cythonize
+
+
+
+setup(
+    name = "my cython modules",
+    author='amiehe',
+    author_email='amieheessomba@etu.unistra.fr',
+    ext_modules=cythonize( 
+        [
+            "making_stat.pyx", 
+            "loop_for.pyx",
+            'NumeriCal.pyx',
+            'arithmetic_analyze.pyx',
+            'fileError.pyx',
+            'making_arr.pyx',
+            'bm_statistics.pyx'
+            ], 
+        annotate = True
+        )
+
+)
