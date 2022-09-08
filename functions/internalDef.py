fca13b0c297d4164791c27117584e1a9a1192470
Iréné <amieheessomba@etu.unistra.fr>
2022-09-08 10:29:20 +0200

    updating functions

--- functions/internalDef.py
+++ functions/internalDef.py
@@ -52,6 +52,7 @@ class INTERNAL_BLOCKS:
         self._return_           = None
         self.error              = None
         self.value              = None
+
         self.string             = self.string[  self.back_end : ]
         self.normal_string      = self.normal_string[ self.back_end : ]
         self.badFunctions       = ['elif', 'else', 'except', 'finally', 'case', 'default', 'func', 'class']
@@ -63,115 +64,118 @@ class INTERNAL_BLOCKS:
 
             if self.error is None:
                 try:
-                    if   self.normal_string[: 2] == 'if'        :
-                        if self.normal_string[-1] == ':':
-                            if self.normal_string[2] in [' ']:
-                                self._return_ = 'if:'
-                                self.value, self.error = MS.MAIN(self.normal_string, self.data_base, self.line).MAIN(
-                                    typ='if', opposite=False, interpreter=interpreter, function=function)
-                            else: self.error = er.ERRORS(self.line).ERROR5(self.normal_string)
-                        else:
-                            try:
-                                if self.normal_string[2] in [' ']:  self.error = er.ERRORS(self.line).ERROR1('if')
-                                else:
-                                    self._return_ = 'any'
-                                    self.value = self.normal_string
-                            except IndexError: self.error = er.ERRORS(self.line).ERROR1('if')
-                    elif self.normal_string[: 3] == 'for'       :
-                        self._return_, self.value, self.error = mainFor.FOR_BLOCK(self.normal_string,
-                                              self.data_base, self.line).FOR( function=function, interpreter=interpreter)
-                    elif self.normal_string[: 6] == 'unless'    :
-                        if self.normal_string[-1] == ':':
-                            if self.normal_string[6] in [' ']:
-                                self._return_ = 'unless:'
-                                self.value, self.error = MS.MAIN(self.normal_string, self.data_base, self.line).MAIN(
-                                    typ='unless', opposite=True, interpreter=interpreter, function=function)
-                            else: self.error = er.ERRORS(self.line).ERROR5(self.normal_string)
-                        else:
-                            try:
-                                if self.normal_string[6] in [' ']: self.error = er.ERRORS(self.line).ERROR1('if')
-                                else:
-                                    self._return_ = 'any'
-                                    self.value = self.normal_string
-                            except IndexError:  self.error = er.ERRORS(self.line).ERROR1('unless')
-                    elif self.normal_string[: 5] == 'until'     :
-                        if self.normal_string[-1] == ':':
-                            if self.normal_string[5] in [' ']:
-                                self._return_ = 'until:'
-                                self.value, self.error = MS.MAIN(self.normal_string, self.data_base, self.line).MAIN(
-                                    typ='until', opposite=True, interpreter=interpreter, function=function)
-                            else:  self.error = er.ERRORS(self.line).ERROR5(self.normal_string)
-                        else:
-                            try:
-                                if self.normal_string[5] in [' ']:  self.error = er.ERRORS(self.line).ERROR1('until')
-                                else:
-                                    self._return_ = 'any'
-                                    self.value = self.normal_string
-                            except IndexError: self.error = er.ERRORS(self.line).ERROR1('until')
-                    elif self.normal_string[: 5] == 'while'     :
-                        if self.normal_string[-1] == ':':
-                            if self.normal_string[5] in [' ']:
-                                self._return_ = 'while:'
-                                self.value, self.error = MS.MAIN(self.normal_string, self.data_base, self.line).MAIN(
-                                    typ='while', opposite=False, interpreter=interpreter, function=function)
-                            else: self.error = er.ERRORS(self.line).ERROR5(self.normal_string)
-                        else:
-                            try:
-                                if self.normal_string[5] in [' ']:  self.error = er.ERRORS(self.line).ERROR1('while')
-                                else:
-                                    self._return_ = 'any'
-                                    self.value = self.normal_string
-                            except IndexError: self.error = er.ERRORS(self.line).ERROR1('while')
-                    elif self.normal_string[: 6] == 'switch'    :
-                        if self.normal_string[-1] == ':':
-                            if self.normal_string[6] in [' ']:
-                                self._return_ = 'switch:'
-                                self.value, self.error = MS.MAIN(self.normal_string, self.data_base, self.line).MAIN(
-                                    typ='switch', opposite=False, interpreter=interpreter, function=function)
-                            else:  self.error = er.ERRORS(self.line).ERROR5(self.normal_string)
-                        else:
-                            try:
-                                if self.normal_string[6] in [' ']:  self.error = er.ERRORS(self.line).ERROR1('switch')
-                                else:
-                                    self._return_ = 'any'
-                                    self.value = self.normal_string
-                            except IndexError: self.error = er.ERRORS(self.line).ERROR1('switch')
-                    elif self.normal_string[: 3] == 'try'       :
-                        self._return_, self.value, self.error = structure.STRUCT().STRUCT(num=3,
-                                                                                          normal_string=self.normal_string)
-                    elif self.normal_string[: 5] == 'begin'     :
-                        self._return_, self.value, self.error = structure.STRUCT().STRUCT(num=5,
-                                                                                          normal_string=self.normal_string)
-                    elif self.normal_string[: 3] == 'def'       :
-                        if self.normal_string[ 3 ] == ' ':
-                            self._return_   = 'def:'
-                            self.value      = self.normal_string
-                        else:
-                            self._return_   = 'any'
-                            self.value      = 't'*self.back_end+self.normal_string
-                    elif self.normal_string[: 3] == 'end'       :
-                        if loop is True:
+                    if self.normal_string[ 0 ] != '#':
+                        if   self.normal_string[: 2] == 'if'        :
                             if self.normal_string[-1] == ':':
-                                self._return_   = 'end:'
-                                self.error      = externalBlocks.EXTERNAL(self.data_base, self.line).EXTERNAL(snum=3,
-                                                                                                 normal_string=self.normal_string)
+                                if self.normal_string[2] in [' ']:
+                                    self._return_ = 'if:'
+                                    self.value, self.error = MS.MAIN(self.normal_string, self.data_base, self.line).MAIN(
+                                        typ='if', opposite=False, interpreter=interpreter, function=function)
+                                else: self.error = er.ERRORS(self.line).ERROR5(self.normal_string)
                             else:
-                                if self.normal_string in ['end']: self.error = er.ERRORS(self.line).ERROR1('end')
-                                else: self.error = er.ERRORS(self.line).ERROR4()
-                        else: self.error = er.ERRORS(self.line).ERROR4()
+                                try:
+                                    if self.normal_string[2] in [' ']:  self.error = er.ERRORS(self.line).ERROR1('if')
+                                    else:
+                                        self._return_ = 'any'
+                                        self.value = self.normal_string
+                                except IndexError: self.error = er.ERRORS(self.line).ERROR1('if')
+                        elif self.normal_string[: 3] == 'for'       :
+                            self._return_, self.value, self.error = mainFor.FOR_BLOCK(self.normal_string,
+                                                  self.data_base, self.line).FOR( function=function, interpreter=interpreter)
+                        elif self.normal_string[: 6] == 'unless'    :
+                            if self.normal_string[-1] == ':':
+                                if self.normal_string[6] in [' ']:
+                                    self._return_ = 'unless:'
+                                    self.value, self.error = MS.MAIN(self.normal_string, self.data_base, self.line).MAIN(
+                                        typ='unless', opposite=True, interpreter=interpreter, function=function)
+                                else: self.error = er.ERRORS(self.line).ERROR5(self.normal_string)
+                            else:
+                                try:
+                                    if self.normal_string[6] in [' ']: self.error = er.ERRORS(self.line).ERROR1('if')
+                                    else:
+                                        self._return_ = 'any'
+                                        self.value = self.normal_string
+                                except IndexError:  self.error = er.ERRORS(self.line).ERROR1('unless')
+                        elif self.normal_string[: 5] == 'until'     :
+                            if self.normal_string[-1] == ':':
+                                if self.normal_string[5] in [' ']:
+                                    self._return_ = 'until:'
+                                    self.value, self.error = MS.MAIN(self.normal_string, self.data_base, self.line).MAIN(
+                                        typ='until', opposite=True, interpreter=interpreter, function=function)
+                                else:  self.error = er.ERRORS(self.line).ERROR5(self.normal_string)
+                            else:
+                                try:
+                                    if self.normal_string[5] in [' ']:  self.error = er.ERRORS(self.line).ERROR1('until')
+                                    else:
+                                        self._return_ = 'any'
+                                        self.value = self.normal_string
+                                except IndexError: self.error = er.ERRORS(self.line).ERROR1('until')
+                        elif self.normal_string[: 5] == 'while'     :
+                            if self.normal_string[-1] == ':':
+                                if self.normal_string[5] in [' ']:
+                                    self._return_ = 'while:'
+                                    self.value, self.error = MS.MAIN(self.normal_string, self.data_base, self.line).MAIN(
+                                        typ='while', opposite=False, interpreter=interpreter, function=function)
+                                else: self.error = er.ERRORS(self.line).ERROR5(self.normal_string)
+                            else:
+                                try:
+                                    if self.normal_string[5] in [' ']:  self.error = er.ERRORS(self.line).ERROR1('while')
+                                    else:
+                                        self._return_ = 'any'
+                                        self.value = self.normal_string
+                                except IndexError: self.error = er.ERRORS(self.line).ERROR1('while')
+                        elif self.normal_string[: 6] == 'switch'    :
+                            if self.normal_string[-1] == ':':
+                                if self.normal_string[6] in [' ']:
+                                    self._return_ = 'switch:'
+                                    self.value, self.error = MS.MAIN(self.normal_string, self.data_base, self.line).MAIN(
+                                        typ='switch', opposite=False, interpreter=interpreter, function=function)
+                                else:  self.error = er.ERRORS(self.line).ERROR5(self.normal_string)
+                            else:
+                                try:
+                                    if self.normal_string[6] in [' ']:  self.error = er.ERRORS(self.line).ERROR1('switch')
+                                    else:
+                                        self._return_ = 'any'
+                                        self.value = self.normal_string
+                                except IndexError: self.error = er.ERRORS(self.line).ERROR1('switch')
+                        elif self.normal_string[: 3] == 'try'       :
+                            self._return_, self.value, self.error = structure.STRUCT().STRUCT(num=3,
+                                                                                              normal_string=self.normal_string)
+                        elif self.normal_string[: 5] == 'begin'     :
+                            self._return_, self.value, self.error = structure.STRUCT().STRUCT(num=5,
+                                                                                              normal_string=self.normal_string)
+                        elif self.normal_string[: 3] == 'def'       :
+                            if self.normal_string[ 3 ] == ' ':
+                                self._return_   = 'def:'
+                                self.value      = self.normal_string
+                            else:
+                                self._return_   = 'any'
+                                self.value      = 't'*self.back_end+self.normal_string
+                        elif self.normal_string[: 3] == 'end'       :
+                            if loop is True:
+                                if self.normal_string[-1] == ':':
+                                    self._return_   = 'end:'
+                                    self.error      = externalBlocks.EXTERNAL(self.data_base, self.line).EXTERNAL(snum=3,
+                                                                                                     normal_string=self.normal_string)
+                                else:
+                                    if self.normal_string in ['end']: self.error = er.ERRORS(self.line).ERROR1('end')
+                                    else: self.error = er.ERRORS(self.line).ERROR4()
+                            else: self.error = er.ERRORS(self.line).ERROR4()
+                        else:
+                            if self.normal_string[-1] != ':':
+                                if self.normal_string not in self.badFunctions:
+                                    if class_key is False:
+                                        self._return_ = 'any'
+                                        self.value = 't' * self.back_end + self.normal_string
+                                    elif class_key is True:
+                                        self._return_ = 'any'
+                                        self.value, self.error = SELF_METHOD(self.normal_string, self.data_base, self.line).SELF_METHOD()
+                                        if self.error == None: self.value = 't' * self.back_end + self.value
+                                        else: pass
+                                else:  _, self.error = self.control.CHECK_NAME(self.normal_string)
+                            else: self.error = er.ERRORS(self.line).ERROR0(self.normal_string)
                     else:
-                        if self.normal_string[-1] != ':':
-                            if self.normal_string not in self.badFunctions:
-                                if class_key is False:
-                                    self._return_ = 'any'
-                                    self.value = 't' * self.back_end + self.normal_string
-                                elif class_key is True:
-                                    self._return_ = 'any'
-                                    self.value, self.error = SELF_METHOD(self.normal_string, self.data_base, self.line).SELF_METHOD()
-                                    if self.error == None: self.value = 't' * self.back_end + self.value
-                                    else: pass
-                            else:  _, self.error = self.control.CHECK_NAME(self.normal_string)
-                        else: self.error = er.ERRORS(self.line).ERROR0(self.normal_string)
+                        self._return_ = 'comment_line'
                 except IndexError: self.error = er.ERRORS(self.line).ERROR4()
             else:
                 self._return_   = 'empty'
