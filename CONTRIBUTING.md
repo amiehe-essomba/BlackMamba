94733adda8d2818e44ccd40078c5cb9d086f0d0f
Iréné <amieheessomba@etu.unistra.fr>
2022-09-08 16:41:44 +0200

    new modules

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,2 +1,24 @@
+# Notes for Black Mama contributors
 
-## How contributing to black mamba project.
+Hi ! if you are new in black mamba community : so, you are very welcome and thaks a lot for using BM. Please be sure to respect our community standards in all interactions.
+
+## Learning Black Mamba
+
+## Before filing an issue
+
+## Contributor Checklist 
+
+## how to file a bug report 
+
+## Submiting contributions 
+
+## Code formatting guidelines
+
+## Contribution to the standard library
+
+## improving documentation
+
+## Git recommendations for pull requests
+
+## Realeases
+
---
+++ script/PARXER/parxerError.py
@@ -0,0 +1,36 @@
+from script.STDIN.LinuxSTDIN                            import bm_configure as bm
+try:
+    from CythonModules.Windows                          import fileError as fe 
+except ImportError:
+    from CythonModules.Linux                            import fileError as fe 
+
+class ERRORS:
+    def __init__(self, line):
+        self.line       = line
+        self.cyan       = bm.fg.cyan_L
+        self.red        = bm.fg.red_L
+        self.green      = bm.fg.green_L
+        self.yellow     = bm.fg.yellow_L
+        self.magenta    = bm.fg.magenta_M
+        self.white      = bm.fg.white_L
+        self.blue       = bm.fg.blue_L
+        self.reset      = bm.init.reset
+
+    def ERROR0(self, string: str):
+        error = '{}line: {}{}'.format(self.white, self.yellow, self.line)
+        self.error = fe.FileErrors( 'SyntaxError' ).Errors() +'{}invalid syntax in {}<< {} >> '.format(self.white, self.cyan, string) + error
+
+        return self.error+self.reset
+
+    def ERROR1(self, string: str, _char_ = 'an integer()' ):
+        error = '{}is not {}{} {}type. {}line: {}{}'.format(self.white, self.blue, _char_, self.yellow,
+                                                            self.white, self.yellow, self.line)
+        self.error = fe.FileErrors( 'TypeError' ).Errors() +'{}<< {} >> '.format(self.magenta, string) + error
+
+        return self.error+self.reset
+    
+    def ERROR2(self, func: str ):
+        error = '{}returns no values. {}line: {}{}'.format(self.yellow, self.white, self.yellow, self.line)
+        self.error = fe.FileErrors( 'AttributeError' ).Errors() +'{}{} '.format(self.cyan, func) + error
+
+        return self.error+self.reset
