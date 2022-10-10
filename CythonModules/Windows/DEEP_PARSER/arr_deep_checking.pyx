ecd592b183c53c7569605f23d3c3c8eea8c51912
Iréné <amieheessomba@etu.unistra.fr>
2022-10-10 23:36:46 +0200

    updating black M

---
+++ CythonModules/Windows/DEEP_PARSER/arr_deep_checking.pyx
@@ -0,0 +1,175 @@
+from CythonModules.Windows.DEEP_PARSER   import arr_deep_checking_init as arr
+from script.MATHS                       import  mathematics
+
+class AR_DEEP_CHECKING:
+    cdef public:
+        dict data_base
+        unsigned long int line 
+        dict master 
+        list operators, values
+    cdef :
+        str error 
+        list  index, numeric
+        list _get_values_, calculations
+    
+    def __cinit__(self, master, values, operators, data_base, line):
+        self.master         = master 
+        self.data_base      = data_base 
+        self.line           = line 
+        self.operators      = operators
+        self.values         = values
+        self.error          = ""
+        self.index          = []
+        self.numeric        = []
+        self.calculations   = []
+        self._get_values_   = values
+
+    cdef INIT_INIT(self, str main__main ):
+        cdef:
+            unsigned long int k, idd, s
+            unsigned long int sub_len_val = len( self._get_values_ )
+            unsigned long int sub_len_op  = len( self.operators )
+            dict sub_values
+            str typ , sign, string
+            bint key = False
+            str history_of_op = ""
+
+        if sub_len_val > sub_len_op     :
+            for k in range( len(self._get_values_ )):
+                if type( self._get_values_[ k ] ) == type( dict() ):
+                    sub_values     = self._get_values_[ k ]
+                    typ = sub_values[ 'type' ]
+
+                    self.num, self.error = TYPE( self.master, sub_values, self.data_base, 
+                                        self.line, typ ).TYPE( main__main, name='cython' )
+                    if not self.error:
+                        if k != (sub_len_val - 1):
+                            self.calculations.append( [ self.num[ 0 ] ] )
+                            self.calculations.append( self.operators[ k ] )
+                        else: self.calculations.append( [ self.num[ 0 ] ] )
+                    else: break
+                else:
+                    self.num, self.error = arr.AR_DEEP_CHECKING(self.master, self.data_base,self._get_values_[ k ], 
+                                    self.operators[ 0 ][ k ], self.line).INIT( main__main)
+                                        
+                    if not self.error:
+                        if k != (sub_len_val - 1):
+                            self.calculations.append( [ self.num[ 0 ] ] )
+                            self.calculations.append(self.operators[ k ] )
+                        else: self.calculations.append( [ self.num[ 0 ] ] )
+                    else: break
+
+            if not self.error:
+                history_of_op = ""
+                for k in range(len(self.calculations)):
+                    if type( self.calculations[ k ] ) == type( str() ):
+                        history_of_op += self.calculations[ k ]
+                    else: pass
+
+                self.num, self.error = mathematics.MAGIC_MATH_BASE(self.calculations, self.data_base,
+                                                            history_of_op, self.line).MATHS_OPERATIONS(name = "cython")
+                if self.error is None:
+                    self.numeric.append( self.num[ 0 ] )
+                else: pass
+            else: pass
+
+        elif sub_len_val == sub_len_op  :
+            if type(  self.operators[ 0 ] ) == type( list() ): sign = ''
+            else:
+                sign            = self.operators[ 0 ]
+                self.operators  = self.operators[ 1 : ]
+
+                if sign in [ '-' ]: self.calculations.append( sign )
+                else: pass
+
+            for k in range(len(self._get_values_)):
+                if type( self._get_values_[ k ]) == type( dict() ):
+                    typ       = self._get_values_[ k ][ 'type' ]
+                    self.num, self.error = TYPE( self.master, self._get_values_[ k ], self.data_base, self.line,
+                                                                                typ ).TYPE( main_string, name="cython" )
+                    if not self.error:
+                        if k != (sub_len_val - 1):
+                            self.calculations.append( [ self.num[ 0 ]] )
+                            self.calculations.append( self.operators[ k ] )
+                        else: self.calculations.append( [ self.num[ 0 ] ] )
+                    else: break
+                else:
+                    self.num, self.error = arr.AR_DEEP_CHECKING(self.master, self._get_values_[ k ],
+                                    self.operators[ k ], self.data_base, self.line).INIT( main_string )
+
+                    if not self.error:
+                        if k != (sub_len_val - 1):
+                            self.calculations.append( [ self.num[ 0 ] ] )
+                            self.calculations.append( self.operators[ k ] )
+                        else: self.calculations.append( [ self.num[ 0 ] ] )
+                    else: break
+            if not self.error:
+                history_of_op = ""
+                for k in range(len(self.calculations)):
+                    if type(self.calculations[ k ]) == type(str()):  history_of_op += self.calculations[ k ]
+                    else: pass
+
+                self.num, self.error = mathematics.MAGIC_MATH_BASE(self.calculations, self.data_base,
+                                                        history_of_op, self.line).MATHS_OPERATIONS(name="cython")
+
+                if self.error is None:
+                    self.numeric.append(self.num[ 0 ])
+                else: pass
+            else: pass
+
+        else:
+            if type( self.operators[ 0 ] ) == type( list() ):  sign = '+'
+            else:
+                sign            = self.operators[ 0 ]
+                self.operators  = self.operators[ 1 : ]
+
+                if sign in [ '-' ]: self.calculations.append( self.sign )
+                else: pass
+
+            for k in range(len(self._get_values_)):
+                if type( self._get_values_[ k ] ) == type( dict() ):
+                    typ               = self._get_values_[ k ] [ 'type' ]
+                    self.num, self.error    = TYPE( self.master, self._get_values_[ k ], self.data_base, self.line,
+                                                                                typ ).TYPE( main_string , name='cython')
+
+                    if not self.error:
+                        if k != ( sub_len_val - 1):
+                            try:
+                                self.calculations.append( [ self.num[0]] )
+                                self.calculations.append( self.operators[ k + 2 ] )
+                            except TypeError:
+                                self.calculations.append( [ self.num[0]] )
+                                self.calculations.append( self.operators[k + 1] )
+                            except IndexError:
+                                self.calculations.append( [ self.num[0] ] )
+                                self.calculations.append( self.operators[ k ] )
+                        else:  self.calculations.append( [ self.num[0] ] )
+                    else: break
+                else:
+                    self.num, self.error = arr.AR_DEEP_CHECKING(self.master, self._get_values_[k],
+                                                self.operators[ k ], self.data_base, self.line).INIT( main_string )
+
+                    if not self.error:
+                        if k != (sub_len_val-1):
+                            try:
+                                self.calculations.append( [ self.num[ 0 ] ] )
+                                self.calculations.append( self.operators[ k + 1 ] )
+                            except TypeError:
+                                self.calculations.append( [ self.num[ 0 ] ] )
+                                self.calculations.append( self.operators[ k + 2 ] )
+                        else:  self.calculations.append( [ self.num[ 0 ] ] )
+                    else: break
+            if not self.error :
+                history_of_op = ""
+                for k in range(len(self.calculations)):
+                    if type( self.calculations[k] ) == type( str() ):  history_of_op += self.calculations[k]
+                    else: pass
+
+                self.num , self.error = mathematics.MAGIC_MATH_BASE( self.calculations, self.data_base,
+                                                        history_of_op, self.line).MATHS_OPERATIONS(name="cython")
+                if self.error is None:
+                    self.numeric.append( self.num[0] )
+                else: pass
+            else: pass
+
+        return self.numeric, self.error
---
+++ CythonModules/Windows/DEEP_PARSER/arrithmetic_checking.pyx
@@ -0,0 +1,211 @@
+from CythonModules.Windows.DEEP_PARSER import arr_deep_checking_init as arr
+
+
+cdef class NUMERICAL:
+    cdef public:
+        unsigned long int line 
+        dict master, data_base
+    def:
+        list value, arithmetic_operator, numeric, calculations, _return_
+        dict get_values, _get_values_
+        str error, type
+        
+    def __cinit__(self, master, data_base, line ):
+            self.line               = line 
+            self.master             = master
+            self.data_base          = data_base
+            self.value              = []
+            self.arithmetic_operator= []
+            self.calculations       = []
+            self.numeric            = []
+            self.error              = ''
+            self._return_           = []
+            self.get_values         = {}
+            self._get_values_       = {}
+            self.type               = ''
+
+    def ARITHMETIC_CHECKING(self, list values, list arithmetic):
+        self.values                     = values
+        self.arithmetic_operator        = arithmetic
+
+        cdef:
+            unsigned int i , j, w
+            unsigned long int len_val
+            str history_of_op, string, sign
+            list __values__, operators
+            bint key
+            unsigned long long int length_values = len(self.arithmetic_operator)
+
+            history_of_op = ""
+
+        for i in range(length_values):
+            if self.arithmetic_operator[i] is None:
+                self.get_values             = self.values[ i ][ 0 ]
+                self.type                   = self.get_values[ 'type' ]
+
+                self._return_, self.error   = TYPE( self.master, self.get_values, self.data_base, self.line, 
+                                            self.type ).TYPE( main__main, name="cython" )
+                if not self.error : self.numeric.append( self._return_[ 0 ] )
+                else:  break
+            else:
+                if len( self.values[ i ] ) > len( self.arithmetic_operator[i] ):
+                    len_val                             = len( self.values[ i ] )
+                    for j in range( len_val ):
+                        self.type                        = self.values[ i ][ j ][ 'type' ]
+
+                        self._return_, self.error       = TYPE( self.master, self.values[ i ][ j ], self.data_base,
+                                                          self.line, self.type ).TYPE( main__main, name="cython" )
+                        if  not self.error:
+                            if j != len_val - 1:
+                                self.calculations.append( [ self._return_[0] ] )
+                                self.calculations.append( self.arithmetic_operator[i][ j ] )
+                            else: self.calculations.append( [ self._return_ [0]] )
+                        else:  break
+                
+                    if not  self.error:
+                        history_of_op   = ''
+                        for w in range(len(self.calculations)):
+                            if type( self.calculations[w] ) == type( str() ):
+                                history_of_op += self.calculations[w]
+                            else:  pass
+
+                        __values__, self.error       = mathematics.MAGIC_MATH_BASE( self.calculations, self.data_base,
+                                                                history_of_op, self.line ).MATHS_OPERATIONS(name="cython")
+                        if not self.error:
+                            self.numeric.append( __values__[ 0 ] )
+                            self.calculations = []
+                        else:   break
+                    else:  break
+                elif len( self.values[ i ] ) == len( self.arithmetic_operator[i] ):
+                    if type( self.arithmetic_operator[i][ 0 ] ) == type( list() ):  sign = '+'
+                    else:
+                        if len( self.arithmetic_operator[i] ) == 1:  sign = self.arithmetic_operator[i][ 0 ]
+                        else:
+                            key = False
+                            for w in range(len(self.arithmetic_operator[i]))
+                                if type( self.arithmetic_operator[i][w] ) == type( list( ) ):
+                                    key = True
+                                    break
+                                else: pass
+
+                            if key == False: sign = self.arithmetic_operator[i][ 0 ]
+                            else:  sign = ''
+
+                    if sign in [ '-' ]: self.calculations.append( sign )
+                    else:  pass
+
+                    len_val = len( self.values[ i ] )
+
+                    if len( self.arithmetic_operator[i] ) == 1:
+                        self._get_values_ = self.values[ i ][ 0 ]
+
+                        try:
+                            self.type                   = self._get_values_[ 'type' ]
+                            self._return_, self.error   = TYPE( self.master, self._get_values_, self.data_base, self.line,
+                                                                              self.type ).TYPE( main__main, name='cython' )
+                            if not self.error :
+                                self.calculations.append( [ self._return_[ 0 ] ] )
+                                history_of_op      = ''
+                                for w in range(len(self.calculations)):
+                                    if type( self.calculations[w] ) == type( str() ): history_of_op += self.calculations[w]
+                                    else:  pass
+
+                                __values__, self.error = mathematics.MAGIC_MATH_BASE( self.calculations,
+                                                    self.data_base, history_of_op, self.line ).MATHS_OPERATIONS(name='cython')
+
+                                if not self.error:
+                                    self.numeric.append( __values__[ 0 ] )
+                                    self.calculations = []
+                                else:  break
+                            else : break
+
+                        except TypeError :
+                            self._return_, self.error   = arr.AR_DEEP_CHECKING( self.master, self._get_values_,
+                                        self.arithmetic_operator[i][ 0 ], self.data_base, self.line ).INIT( main__main )
+                            if not self.error:
+                                self.calculations.append( [ self._return_[ 0 ] ])
+                                history_of_op      = ''
+                                for w i range(len(self.calculations)):
+                                    if type( self.calculations[w] ) == type( str() ):
+                                        history_of_op += self.calculations[w]
+                                    else:  pass
+                            
+                                __values__, self.error = mathematics.MAGIC_MATH_BASE( self.calculations,
+                                                    self.data_base, history_of_op, self.line ).MATHS_OPERATIONS(name='cython')
+
+                                if not self.error:
+                                    self.numeric.append( __values__[ 0 ] )
+                                    self.calculations = []
+                                else:  break
+                            else: break
+                    else:
+                        for j in range( len_val ):   
+                            if type( self.values[ i ][ j ] ) == type( dict() ) :
+                                self._get_values_ = self.values[ i ][ j ]
+                                self.type = self._get_values_[ 'type' ]
+                                self._return_, self.error = TYPE( self.master, self._get_values_, self.data_base, self.line,
+                                                            self.type ).TYPE( main__main, name='cython' )
+                                if not self.error:
+                                    if j != self.len_val - 1:
+                                        try:
+                                            self.calculations.append( [ self._return_[ 0 ] ] )
+                                            if self.calculations[ 0 ] == '-':  
+                                                self.calculations.append( self.arithmetic_operator[i][ j + 1 ] )
+                                            else:  self.calculations.append(self.arithmetic_operator[i][ j + 1 ])
+                                        except TypeError:
+                                            self.calculations.append( [ self._return_[ 0 ] ] )
+                                            self.calculations.append( self.arithmetic_operator[i][ j ] )
+                                    else:  self.calculations.append( [ self._return_[ 0 ] ] )
+                                else: break
+                            else:
+                                self._return_, self.error = arr.AR_DEEP_CHECKING(self.master, self.values[ i ][ j ],
+                                                self.arithmetic_operator[i][ j ], self.data_base, self.line).INIT( main__main )
+
+                                if not self.error:
+                                    if j != len_val - 1:
+                                        try:
+                                            self.calculations.append( [ self._return_[ 0 ] ] )
+                                            self.calculations.append( self.arithmetic_operator[i][ j + 1 ] )
+                                        except TypeError:
+                                            self.calculations.append( [ self._return_[ 0 ] ] )
+                                            self.calculations.append( self.arithmetic_operator[i][ j ] )
+                                    else:  self.calculations.append( [ self._return_[ 0 ]] )
+                                else: break
+                        if not self.error:
+                            history_of_op  = ''
+                            for w in range(len(self.calculations)):
+                                if type( self.calculations[w] ) == type( str() ):  history_of_op += self.calculations[w]
+                                else: pass
+
+                            __values__, self.error = mathematics.MAGIC_MATH_BASE(self.calculations,
+                                                self.data_base, history_of_op, self.line) .MATHS_OPERATIONS(name="cython")
+
+                            if self.error is None:
+                                self.numeric.append( __values__[ 0 ] )
+                                self.calculations = []
+                            else: break
+                        else:  break
+                
+                else:
+                    sign               = ''
+             
+                    self._return_, self.error = arr.AR_DEEP_CHECKING(self.master, self.values[ i ],
+                                                       self.arithmetic_operator[i], self.data_base, self.line).INIT( main__main )
+
+                    if not self.error:
+                        history_of_op  =  ''
+                        self.calculations.append( [ self._return_[ 0 ]] )
+                        for w in range(len(self.calculations)):
+                            if type( self.calculations[w] ) == type( str() ):  history_of_op += self.calculations[w]
+                            else:  pass
+                        
+                        __values__, self.error = mathematics.MAGIC_MATH_BASE(self.calculations,
+                                            self.data_base, history_of_op, self.line).MATHS_OPERATIONS(name = 'cython')
+
+                        if not self.error:
+                            self.numeric.append( __values__[ 0 ][ 0 ] )
+                            self.calculations = []
+                        else:  break
+                    else:  break
+        
+        return  self.numeric, self.error
