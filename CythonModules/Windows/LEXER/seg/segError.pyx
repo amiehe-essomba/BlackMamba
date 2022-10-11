9e7e404e163415cf5fcaf0c7fd14b83544b724b9
Iréné <amieheessomba@etu.unistra.fr>
2022-10-11 23:24:04 +0200

    updating

--- CythonModules/Windows/LEXER/seg/segError.pyx
+++ CythonModules/Windows/LEXER/seg/segError.pyx
@@ -19,53 +19,53 @@ cdef class ERROR:
         self.reset      = bm.init.reset
         self.error      = ''
 
-    cdef ERROR0(self, string: str ):
+    cdef str ERROR0(self, str string ):
         self.error = ' {}line: {}{}'.format( self.white, self.yellow, self.line )
         self.error = fe.FileErrors( 'SyntaxError' ).Errors()+'{}invalid syntax in {}<< {} >>.'.format(self.white, self.cyan, string)+self.error
 
         return self.error+self.reset
 
-    cdef ERROR1(self, string: str ):
+    cdef str ERROR1(self, str string ):
         self.error      = '{}due to {}<< , >> {}at the beginning. {}line: {}{}'.format(self.white, self.red, self.yellow, 
                                                                                    self.white, self.yellow,self.line)
         self.error  = fe.FileErrors( 'SyntaxError' ).Errors()+'{}invalid syntax, in {}<< {} >> '.format(self.white, self.cyan, string) + self.error
 
         return self.error+self.reset
 
-    cdef ERROR2(self):
+    cdef str ERROR2(self):
         self.error       = '{}<< , >> {}on the previous line. {}line: {}{}'.format(self.red, self.yellow, 
                                                                               self.white,self.yellow, (self.line-1) )
         self.error  = fe.FileErrors( 'SyntaxError' ).Errors()+'{}invalid syntax, due to '.format(self.white) + self.error
 
         return self.error+self.reset
 
-    cdef ERROR3(self):
+    cdef str ERROR3(self):
         self.error       = '{}<< , >> {}was not set on the previous line. {}line: {}{}'.format(self.red, self.yellow,
                                                                                           self.white, self.yellow, (self.line - 1) )
         self.error  = fe.FileErrors( 'SyntaxError' ).Errors()+'{}invalid syntax '.format(self.white) + self.error
 
         return self.error+self.reset
 
-    cdef ERROR4(self, _open_: str, _close_: str):
+    cdef str ERROR4(self, str _open_, str _close_):
         self.error      = '{}<< {} {} >> {}line: {}{}'.format(self.blue, _open_,  _close_, 
                                                           self.white, self.yellow, self.line)
         self.error  = fe.FileErrors( 'SyntaxError' ).Errors()+'{}invalid syntax, due no value inside '.format(self.white) + self.error
 
         return  self.error+self.reset
 
-    cdef ERROR5(self, string: str):
+    cdef str ERROR5(self, str string):
         self.error      = '{}due to no value {}before << {},{} >>. line: {}{}'.format(self.white, self.red, self.green, self.white, self.yellow, self.line )
         self.error  = fe.FileErrors( 'SyntaxError' ).Errors()+ '{}invalid syntax in {} << {} >>, '.format(self.white, self.cyan, string) + self.error
 
         return self.error+self.reset
 
-    cdef ERROR6(self):
+    cdef str ERROR6(self):
         self.error       = '{}<< , >> {} at the end on the previous line. line: {}{}'.format(self.red, self.white, self.yellow, (self.line - 1))
         self.error  = fe.FileErrors( 'SyntaxError' ).Errors()+'{}invalid syntax, due to '.format(self.white) + self.error
 
         return  self.error+self.reset
 
-    cdef ERROR7(self, mains_tring: str, sub_string: str):
+    cdef str ERROR7(self, str mains_tring, str sub_string):
 
         self.error = '{}due to bad {}char, {}<< {} >>. {}line: {}{}'.format(self.white, self.red, self.green, sub_string, 
                                                                        self.white, self.yellow, self.line)
@@ -73,40 +73,40 @@ cdef class ERROR:
 
         return self.error+self.reset
 
-    cdef ERROR8(self):
+    cdef str ERROR8(self):
         self.error = fe.FileErrors( 'SyntaxError' ).Errors()+'{}syntax error, {}EMPTY {}value was detected. {}line : {}{}'.format(self.white,
                                                     self.green, self.yellow, self.white, self.yellow, self.line)
 
         return  self.error+self.reset
 
-    cdef ERROR9(self):
+    cdef str ERROR9(self):
         self.error = fe.FileErrors( 'IndentationError' ).Errors() + '{}unexpected an indented block. {}line : {}{}'.format(self.yellow, 
                                                 self.white, self.yellow, self.line)
 
         return self.error+self.reset
 
-    cdef ERROR_TREATMENT1(self, string: str, _open_: str):
+    cdef str ERROR_TREATMENT1(self, str string, str _open_):
         self.error      = '{}close {}the {}opening {}<< {} >>. {}line: {}{}'.format(self.green, self.white, self.red, self.blue,
                                                                         _open_, self.white, self.yellow, self.line)
         self.error  = fe.FileErrors( 'SyntaxError' ).Errors()+'{}invalid syntax in {}<< {} >> '.format(self.white, self.cyan, string) + self.error
 
         return self.error+self.reset
 
-    cdef ERROR_TREATMENT2(self, string: str, _close_: str):
+    cdef str ERROR_TREATMENT2(self, str string, str _close_):
         self.error       = '{}open {}<< {} >> {}before {}closing. {}line: {}{}'.format(self.green, self.blue, _close_, self.white, self.red, 
                                                                                   self.white, self.yellow, self.line)
         self.error  = fe.FileErrors( 'SyntaxError' ).Errors()+'{}invalid syntax in {}<< {} >> '.format(self.white, self.cyan, string) + self.error
 
         return self.error+self.reset
 
-    cdef ERROR_TREATMENT3(self, string: str):
+    cdef str ERROR_TREATMENT3(self, str string):
         self.error       = '{}due to, too much  {}<< " >> {}characters. {}line: {}{}'.format(self.white, self.red, self.yellow,
                                                                                 self.white, self.yellow, self.line)
         self.error  = fe.FileErrors( 'SyntaxError' ).Errors()+'{}invalid syntax in {}<< {} >> '.format(self.white, self.cyan, string) + self.error
 
         return self.error+self.reset
 
-    cdef ERROR_TREATMENT4(self, string: str, _open_: str):
+    cdef str ERROR_TREATMENT4(self, str string, str _open_):
         self.error       = '{}due to many {}opening {}<< {} >>. {}line: {}{}'.format(self.white, self.green, self.red, _open_, 
                                                                                 self.white, self.yellow, self.line)
         self.error  = fe.FileErrors( 'SyntaxError' ).Errors()+'{}invalid syntax in {}<< {} >> '.format(self.white, self.cyan, string) + self.error
